d464ac62e4ce48a37dc0d0473fcf6042da6aa1fe
khyatijha777 <khyatijha777@gmail.com>
2020-03-04 23:49:37 +0530

    second

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
