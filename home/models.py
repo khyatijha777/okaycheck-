4e4c3c13dc4a7657ef473b3cb565ccf25d353e9d
khyatijha777 <khyatijha777@gmail.com>
2020-03-06 15:05:42 +0530

    models added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from login.models import Login
 # Create your models here.
+class ToDo(models.Model):
+    todo_id =  models.AutoField(primary_key =True)
+    date_created = models.DateField()
+    time_created =  models.TimeField()
+    data = models.TextField(blank = True)
+    done = models.BooleanField()
+    complete_by_date = models.DateField(auto_now=False, auto_now_add=False)
+    complete_by_time = models.TimeField( auto_now=False, auto_now_add=False)
+    u_id = models.ForeignKey(Login, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.todo_id 
--- login/models.py
+++ login/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+class Login(models.Model):
+    u_id = models.AutoField(primary_key =True)
+    first_name = models.CharField(max_length=20)
+    last_name =  models.CharField(max_length=20)
+    user_name =  models.CharField( max_length=30)
+    email = models.EmailField( max_length=254, )
+    password = models.CharField(max_length=20)
+    conatct_no =  models.CharField(max_length=10)
+    def __str__(self):
+        return self.user_name
